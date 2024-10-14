556df14fc461a31dfa74d01641adbedb5b585549
JDiaz <jdiaz@agrocolor.es>
2024-10-14 11:59:44 +0200

    Changes

--- src/RestApi.php
+++ src/RestApi.php
@@ -2,20 +2,33 @@
 
 namespace Jdiaz\Rest;
 
-class RestApi {
+class RestApi
+{
 
-    public CONST POST = "POST";
-    public CONST PUT = "PUT";
-    public CONST GET = "GET";
-    public CONST DELETE = "DELETE";
-    public CONST JSON_CONTENT_TYPE = 'Content-Type: application/json';
+    public const POST = 'POST';
+    public const PUT = 'PUT';
+    public const GET = 'GET';
+    public const DELETE = 'DELETE';
 
-    public function send(string $url, ?array $header, array $params, array $body, string $contentType, string $requestType): array 
+    public const JSON_CONTENT_TYPE = 'Content-Type: application/json';
+    public const WWW_FORM_URLENCODED_TYPE = 'Content-Type: application/x-www-form-urlencoded';
+
+    public static function send(string $url, ?array $header, array $params, array $body, string $contentType, string $requestType): array
     {
         $curl = curl_init();
 
         if (!empty($params)) {
             $url .= '?' . http_build_query($params);
+        }
+
+        $payload = http_build_query($body);
+
+        if ($contentType == self::JSON_CONTENT_TYPE) {
+            $payload = json_encode($body);
+        }
+
+        if ($contentType == self::WWW_FORM_URLENCODED_TYPE) {
+            $payload = http_build_query($body);
         }
 
         curl_setopt_array($curl, [
@@ -27,20 +40,20 @@ class RestApi {
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $requestType,
-            CURLOPT_POSTFIELDS => json_encode($body),
-            CURLOPT_HTTPHEADER => $header ? $header : [$contentType]
+            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_HTTPHEADER => $header ? $header : [$contentType],
         ]);
 
         $response = curl_exec($curl);
 
-        if (curl_errno($curl)) { 
-            print curl_error($curl); 
-         } 
+        if (curl_errno($curl)) {
+            print curl_error($curl);
+        }
 
         curl_close($curl);
 
         return json_decode($response, true);
-        
+
     }
 
 }
